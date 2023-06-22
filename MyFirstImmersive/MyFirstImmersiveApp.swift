133f0d5f467a6edb41f772cd2896f40d6b1a80c4
chavalk <chavalk@hotmail.com>
2023-06-22 11:11:58 -0500

    First commit.

---
+++ MyFirstImmersive/MyFirstImmersiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstImmersiveApp.swift
+//  MyFirstImmersive
+//
+//  Created by Jose Garcia on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstImmersiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
